b69f1fff5ff18f4a3d470530ce71c35873144a54
Prashant Ghimire <111095724+Prsnt95@users.noreply.github.com>
2024-09-07 17:19:44 -0500

    update flashcard

--- src/pages/FlashCard/SectionPage.jsx
+++ src/pages/FlashCard/SectionPage.jsx
@@ -1,14 +1,12 @@
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import ReplyRoundedIcon from "@mui/icons-material/ReplyRounded";
-import { TextField } from "@mui/material";
-import { useState } from "react";
-import { Card } from "@mui/material";
-
+import { TextField, Card } from "@mui/material";
+import { useEffect, useState } from "react";
+import LoopIcon from "@mui/icons-material/Loop";
 const SectionPage = () => {
   const { section, id } = useParams();
 
-  const [flashcards, setFlashCards] = useState([{ front: "", back: "" }]);
+  const [flashcards, setFlashCards] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,22 +18,39 @@ const SectionPage = () => {
     setFlashCards((prev) => [...prev, newFlashcard]);
     e.target.reset(); // Clear form after submission
   };
+
+  useEffect(() => {
+    const savedFlashcards = localStorage.getItem(`FlashCards_${id}`);
+    if (savedFlashcards) {
+      const parsedFlashcards = JSON.parse(savedFlashcards);
+      if (parsedFlashcards.length > 0) {
+        setFlashCards(parsedFlashcards);
+      }
+    }
+  }, [id]);
+
+  useEffect(() => {
+    localStorage.setItem(`FlashCards_${id}`, JSON.stringify(flashcards));
+  }, [flashcards, id]);
+
   return (
     <div className="sectionPage--wrapper">
       <div className="sectionPage--header">
         <Link to={`/Flashcard`}>
-          <ReplyRoundedIcon></ReplyRoundedIcon>
+          <ReplyRoundedIcon />
         </Link>
         Flashcards
         <h1>{section}</h1>
         <p>Card ID: {id}</p>
       </div>
       <div className="sectionPage--flashcardWrapper">
-        <div className="sectionPage--flashcard"></div>
+        <div className="sectionPage--flashcard">
+          <LoopIcon />
+        </div>
       </div>
       <form onSubmit={handleSubmit} className="sectionPage--addCards">
-        <TextField name="front" placeholder="Front" required="true" />
-        <TextField name="back" placeholder="Back" />
+        <TextField name="front" placeholder="Front" required />
+        <TextField name="back" placeholder="Back" required />
         <button type="submit">Add Card</button>
       </form>
       {flashcards.map((flashcard, index) => (
